d3e295e1c6871e5492bf9bd9dc8e16c3811e948e
Victor Augusto <victor.soares@irricontrol.com.br>
2022-09-02 13:59:41 -0300

    create task model

--- multiple_database/task/admin.py
+++ multiple_database/task/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from multiple_database.task.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('name',)
